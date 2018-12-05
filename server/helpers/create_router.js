004940f3a8c0c2d53e500a502a39bf3285d673e9
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-05 11:19:02 +0000

    delete route added to create_router.js

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -38,6 +38,19 @@ const createRouter = function (collection) {
       .then((docs) => res.json(docs));
   });
 
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: ObjectID(id)})
+      .then(() => collection.find().toArray())
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error:err });
+      });
+  })
+
   return router;
 
 };
