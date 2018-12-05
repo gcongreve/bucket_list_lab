c6cbb4a50ac6fe68ba57505b7daeb8dd74b74001
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-05 11:36:17 +0000

    html added

---
+++ client/src/helpers/pub_sub.js
@@ -0,0 +1,17 @@
+const PubSub = {
+  publish: function (channel, payload) {
+    const event = new CustomEvent(channel, {
+      detail: payload
+    });
+    console.log(`pubsub is publishing on ${channel} with payload ${payload}`);
+    document.dispatchEvent(event);
+  },
+
+  subscribe: function (channel, callback) {
+    document.addEventListener(channel, callback);
+  }
+  console.log(`pubsub is subscribing on ${channel}`);
+
+};
+
+module.exports = PubSub;
